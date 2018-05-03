a4becafa47b8d866a014aeb9026b49178fb2a3d0
Morinventiv <brandonmorin@gmail.com>
2018-05-02 22:11:58 -0600

    added ORM and controller

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,21 @@
 var connection = require('../config/connection.js');
+
+module.exports = {
+	selectAll: function (table, cb) {
+		connection.query('SELECT * FROM ??', [table], function (err, data) {
+			cb(data);
+		})
+	},
+	createNew: function (table, cb) {
+		connection.query('INSERT INTO burgers (burger) VALUES (?);', [table], function (err, data) {
+			if (err) throw err;
+			cb(data);
+		})
+	},
+	update: function (burger, id, cb) {
+		connection.query('UPDATE burgers SET burger = ? WHERE id = ?;', [burger, id], function (err, data) {
+			if (err) throw err;
+			cb(data);
+	})
+}
+}
